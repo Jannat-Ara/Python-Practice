adba4ff3aa6d8dc2300baf5a145e5fd2acb2ccf1
Jannat-Ara-Ferdouse <59131687+Jannat-Ara@users.noreply.github.com>
2023-02-21 00:03:24 +0600

    A better way to set dimension

---
+++ College_algebra_ft_by_freeCodeCamp/Graphs/A_better_way_to_set_dim.py
@@ -0,0 +1,12 @@
+# a better way to set dimension
+import matplotlib.pyplot as plt
+ 
+
+xmin= -10
+xmax= 10
+ymin= -10
+ymax= 10
+
+fig, ax = plt.subplots()
+plt.axis([xmin, xmax, ymin,ymax])
+plt.show()
