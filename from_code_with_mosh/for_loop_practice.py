bae491892f5487339c1a5190510664b68f97896e
Jannat-Ara-Ferdouse <59131687+Jannat-Ara@users.noreply.github.com>
2023-03-04 11:55:40 +0600

    For loop practice

---
+++ from_code_with_mosh/for_loop_practice.py
@@ -0,0 +1,9 @@
+# Find the total cost of all the items in the shopping cart
+
+price_of_items = [10,20, 30]
+total = 0
+
+for price in price_of_items:
+    total += price
+ 
+print(f"Total price: {total}")
