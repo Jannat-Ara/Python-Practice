c4f131c661a68414af7496fd737ebc5dc772fc2a
Jannat-Ara-Ferdouse <59131687+Jannat-Ara@users.noreply.github.com>
2023-03-04 11:52:19 +0600

    for loops

---
+++ from_code_with_mosh/for_loop.py
@@ -0,0 +1,14 @@
+for item in "Python":
+    print(item)
+
+for name in ["Raya","Harry","Ron"]:
+    print(name)
+
+for numbers in range(10):
+    print(numbers)# initial value is 0 not 1
+
+for n in range(5,10):
+    print(n) # start from 5 and end before 10 which mean ends at 9
+
+for n in range( 5, 10 ,2):
+    print(n)
