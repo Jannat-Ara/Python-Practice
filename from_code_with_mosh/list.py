17041c5e8dbffad653286a937197579799d1b70e
Jannat-Ara-Ferdouse <59131687+Jannat-Ara@users.noreply.github.com>
2023-03-06 09:50:32 +0600

    List

--- from_code_with_mosh/list.py
+++ from_code_with_mosh/list.py
@@ -32,3 +32,9 @@ print(matrix) #[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 print(matrix[0][1])# 2
 
 print(matrix[1][2]) #6
+
+#if you want to print all the item from a 2D matrix
+
+for row in matrix:
+    for item in row:
+        print(item)
