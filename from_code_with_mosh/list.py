3bd4ed79ca585cfef610bbd3ef10e3490fe29183
Jannat-Ara-Ferdouse <59131687+Jannat-Ara@users.noreply.github.com>
2023-03-06 09:49:13 +0600

    List

---
+++ from_code_with_mosh/list.py
@@ -0,0 +1,34 @@
+names =['John','Bob','Mosh','Sarah','Mary']
+print(names[:4]) #['John', 'Bob', 'Mosh', 'Sarah']
+print(names[:])#['John', 'Bob', 'Mosh', 'Sarah', 'Mary']
+print(names[2:])#['Mosh', 'Sarah', 'Mary']
+
+#Exercise: Write a program to find the largest number in a list.
+numbers = [30, 44, 1,25, 2, 72, 15, 7]
+max = numbers[0]
+for number in numbers:
+    if number > max:
+        max = number
+        
+print(max)
+
+#2D list 
+'''
+    1 2 3
+    4 5 6
+    7 8 9
+
+
+'''
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print(matrix) #[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+print(matrix[0][1])# 2
+
+print(matrix[1][2]) #6
