7e391d6cc2ec968671dd38dfde3e05777e2a7139
Jannat-Ara-Ferdouse <59131687+Jannat-Ara@users.noreply.github.com>
2023-03-06 11:07:50 +0600

    List comprehensions

--- Hacker_rank_pratice/List_Comprehensions.py
+++ Hacker_rank_pratice/List_Comprehensions.py
@@ -7,6 +7,18 @@ if __name__ == '__main__':
     new_list = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i+j+k)!=n]
     print(new_list)
 
+    #another way
+    x1 = int(input())
+    y1 = int(input())
+    z1 = int(input())
+    n1 = int(input())
+    new_list1 =[]
 
+    for i in range(x1+1):
+        for j in range(y1+1):
+            for k in range(z1+1):
+                if (i+j+k)!= n1:
+                    new_list1.append([i,j,k])
+    print(new_list1)
 
 
