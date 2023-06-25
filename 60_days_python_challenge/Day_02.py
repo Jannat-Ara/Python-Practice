606bfb41fd5dacfe1c90943e328dee68ebcb26b7
Jannat-Ara-Ferdouse <59131687+Jannat-Ara@users.noreply.github.com>
2023-06-25 21:40:47 +0600

    day_02

--- 60_days_python_challenge/Day_02.py
+++ 60_days_python_challenge/Day_02.py
@@ -1,9 +1,21 @@
 #list are mutable and can be modified after creation
 
 list = [8, 2.3 ,[-4,5], ["apple", "banana"]]
-print(list)
+print(list) #output: [8, 2.3, [-4, 5], ['apple', 'banana']]
 
 #tuples are immutable and can not be modified after creation.
 
 tuple = (("parrot", "sparrow"),("Lion", "Tiger"))
-print(tuple)
+print(tuple) #output:(('parrot', 'sparrow'), ('Lion', 'Tiger'))
+
+
+#Range:
+sequence = range(4, 14, 2)
+for i in sequence:
+    print(i)
+
+
+#dictionary:
+
+dict = {"name":"Raya", "age":24, "canVote":True}
+print(dict) #output: {'name': 'Raya', 'age': 24, 'canVote': True}
