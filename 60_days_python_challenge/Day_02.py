81ff6690de642e187aa030f6f0d6a385c188a604
Jannat-Ara-Ferdouse <59131687+Jannat-Ara@users.noreply.github.com>
2023-06-25 15:35:07 +0600

    day_02

---
+++ 60_days_python_challenge/Day_02.py
@@ -0,0 +1,9 @@
+#list are mutable and can be modified after creation
+
+list = [8, 2.3 ,[-4,5], ["apple", "banana"]]
+print(list)
+
+#tuples are immutable and can not be modified after creation.
+
+tuple = (("parrot", "sparrow"),("Lion", "Tiger"))
+print(tuple)
