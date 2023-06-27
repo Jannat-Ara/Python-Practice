f958210e8e84ad40fe4e11ebbc0b05b989ef8988
Jannat-Ara-Ferdouse <59131687+Jannat-Ara@users.noreply.github.com>
2023-06-27 14:30:08 +0600

    String_problem

---
+++ Hacker_rank_pratice/Simple_string_practice.py
@@ -0,0 +1,6 @@
+def print_full_name(first,last):
+    firstname = first
+    lastname= last
+    print("Hello "+firstname+" "+lastname+"! You just delved into python.")
+
+print_full_name("Ross", "Taylor")
