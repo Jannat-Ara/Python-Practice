624c6fc7fc038e0437fff0c93c463857ee671a66
Jannat-Ara-Ferdouse <59131687+Jannat-Ara@users.noreply.github.com>
2023-06-27 11:44:09 +0600

    Day_04_part_01

---
+++ 60_days_python_challenge/Day_04_part_01.py
@@ -0,0 +1,7 @@
+#Taking User Input in Python
+a =input("Enter the name:")
+print(a)
+
+x = input(" Enter the first number:")
+y = input(" Enter the second number:")
+print(int(x)+ int(y)) # you have to typecast it otherwise the x and y are counted as string and it's gonna concatinate the string and the output will be something like 1217.
