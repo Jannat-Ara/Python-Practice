1657f9f4a953106507dfa6d89e016bab173a084f
Jannat-Ara-Ferdouse <59131687+Jannat-Ara@users.noreply.github.com>
2023-06-30 12:20:02 +0600

    Day_07_part_01

---
+++ 60_days_python_challenge/Day_07_part_01.py
@@ -0,0 +1,9 @@
+import time
+
+timestamp = time.strftime("%H:%M:%S")
+print(timestamp)
+timestamp = time.strftime("%H")
+print("Time in(Hours): "+timestamp)
+
+timestamp = time.strftime("%M")
+print("Time in(Min): "+timestamp)
