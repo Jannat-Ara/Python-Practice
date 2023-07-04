eaf41db2cb1fe73ef3765db00504c63e999b095c
Jannat-Ara-Ferdouse <59131687+Jannat-Ara@users.noreply.github.com>
2023-07-04 23:29:21 +0600

    Day_08_part_02

---
+++ 60_days_python_challenge/Day_08_part_02.py
@@ -0,0 +1,23 @@
+fruits= ["apple","banana","cherry","pineapple"]
+for fruit in fruits:
+    print(fruit)
+    if fruit == "cherry":
+        break        
+    for i in fruit:
+            print(i)
+
+for k in range(5):
+     print(k+1)
+for x in [0, 1, 2]:
+  pass
+
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
+
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
